506e30dda49a403d70a00b01b644cf6dc639b3f8
pptrgi <pptrgitonga@gmail.com>
2023-08-16 18:20:14 +0300

    Replace 'index' with '/' for the home page

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route
                   path="/wishlist"
                   element={
