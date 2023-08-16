b90f4b4ed93e4cc74cd042aa014318d1e4a34f39
pptrgi <pptrgitonga@gmail.com>
2023-08-16 18:28:35 +0300

    Modify file

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Layout />}>
-                <Route path="/" element={<Home />} />
+                <Route index element={<Home />} />
                 <Route
                   path="/wishlist"
                   element={
