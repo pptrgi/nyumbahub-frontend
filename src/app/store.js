c5d00bc880ecfc35c780d520132f465990b022fa
pptrgi <pptrgitonga@gmail.com>
2023-09-14 17:25:51 +0300

    Persist the user's reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import propertyReducer from "../reduxFeatures/properties/propertySlice";
 import categoryReducer from "../reduxFeatures/categories/categorySlice";
 import typeReducer from "../reduxFeatures/types/typeSlice";
 import userReducer from "../reduxFeatures/users/userSlice";
 
-export const store = configureStore({
-  reducer: {
-    property: propertyReducer,
-    category: categoryReducer,
-    type: typeReducer,
-    user: userReducer,
-  },
+// Only persisting the user's reducer. Add it to the whitelist
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+
+// create the root reducer that combines all 4 reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+  category: categoryReducer,
+  property: propertyReducer,
+  type: typeReducer,
 });
+
+// persist the whitelisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// create store persistor and export it
+export const persistor = persistStore(store);
