e71d00903463d6777e136038ca51d61c689deef0
pptrgi <pptrgitonga@gmail.com>
2024-01-10 12:46:33 +0300

    Export header dropdowm menus data

---
+++ src/app/headerItems.js
@@ -0,0 +1,19 @@
+// Define property types and categories to be used in the hamburger and View by Type / View by Category dropdown menus
+
+export const typeDropdownItems = [
+  { name: "apartment", params: "type/64b23dfd3567c004de3bf49e" },
+  { name: "maisonette", params: "type/64b23de73567c004de3bf498" },
+  { name: "villa", params: "type/64b23df23567c004de3bf49b" },
+  { name: "bungalow", params: "type/64b23bd62051ecc3babb05fb" },
+  { name: "mansion", params: "type/64afeefd919371933061ddb4" },
+  { name: "bedsitter", params: "type/64afeefd919371933061ddb4" },
+];
+
+export const categoryDropdownItems = [
+  { name: "for rent", params: "category/64b23f6f3567c004de3bf4c3" },
+  { name: "featured", params: "category/64b23f843567c004de3bf4c9" },
+  { name: "for sale", params: "category/64b23f783567c004de3bf4c6" },
+  { name: "new listing", params: "category/64b23f8f3567c004de3bf4cc" },
+  { name: "top seller", params: "category/64b23fa93567c004de3bf4d2" },
+  { name: "reduced price", params: "category/64b23f983567c004de3bf4cf" },
+];
