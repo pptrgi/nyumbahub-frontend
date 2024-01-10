de953028d054aa4486d27569ab1199808304552f
pptrgi <pptrgitonga@gmail.com>
2024-01-10 12:44:48 +0300

    Remove Strictmode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store, persistor } from "./app/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "react-toastify/dist/ReactToastify.css";
+
+import { store, persistor } from "./app/store";
 import App from "./App";
 import "./index.css";
 
@@ -11,11 +11,9 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
